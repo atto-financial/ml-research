ec1ecb25b46701797ce447355c637a608a042a25
Phichetlog10 <phichetlog10@gmail.comm>
2025-04-28 21:32:40 +0700

    change model name

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,5 @@
 from flask import render_template, request, jsonify
 import pandas as pd
-from app.data.load_data import load_data
-from app.models.train_model import data_split
-from app.models.train_model import train_model
-from app.models.evaluate_model import test_set
-from app.models.evaluate_model import cross_validation
-from app.models.evaluate_model import features_importance
-from app.models.save_model import save_model
 
 def configure_routes(app):
     @app.route('/')
@@ -52,21 +45,28 @@ def configure_routes(app):
               
     @app.route('/train', methods=['POST'])
     def train():
-    
+        
+        from app.data.load_data import load_data
         raw_dat = load_data()
         
+        from app.models.train_model import data_split
         X_train, X_test, y_train, y_test, X, y = data_split(raw_dat)
         
+        from app.models.train_model import train_model
         model = train_model(X_train, y_train)
         
+        from app.models.evaluate_model import test_set
         test_results = test_set(model, X_test, y_test)
         
+        from app.models.evaluate_model import cross_validation
         CV_results = cross_validation(model, X_train, X_test, y_train, y_test, X, y)
-
+       
+        from app.models.evaluate_model import features_importance
         feature_results = features_importance(model, X, y)
         feature_results = feature_results.to_dict(orient='records') if feature_results is not None else []
         
-        save_model(model, "app/models/mlrfth50_v1.2.pkl")
+        #from app.models.save_model import save_model
+        #save_model(model, "app/models/mlrfth50_v1.X.pkl")
         
         combined_results = {
             'test_set': test_results,
@@ -82,7 +82,7 @@ def configure_routes(app):
         new_data = pd.DataFrame([data['cdd_vals']], columns=[f'cdd{i+9}' for i in range(len(data['cdd_vals']))])
 
         from app.predictions.predict import load_model
-        model = load_model("mlrfth50_v1.2")
+        model = load_model("rdf50_m1.0_cdd_f1.0")
 
         from app.predictions.predict import make_predictions
         predictions, probabilities, predictions_adjusted = make_predictions(model, new_data)
